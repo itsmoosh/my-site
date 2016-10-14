45ece219474fe0b947aafe9a15ed17a92e69969d
Marshall <itsmoosh@gmail.com>
2016-10-14 02:14:59 -0700

    Added programmatic individual-page header assignment, giving it a test

--- supporting/scripts.js
+++ supporting/scripts.js
@@ -1,21 +1,27 @@
 "use strict";
 
-function newEval() {
-	var xmlDoc = loadXMLDoc("supporting/studentEvals.xml");
-	var numEvals = xmlDoc.getElementsByTagName('response').length;	
-	var ind = Math.floor(Math.random()*numEvals);	// ind is random integer in the range [0, numEvals)	
-	
-	var currQ = xmlDoc.getElementsByTagName('question')[ind].childNodes[0].nodeValue;	// store a single question and response 
-	var currR = xmlDoc.getElementsByTagName('response')[ind].childNodes[0].nodeValue;	
+/*	Welcome to my lair.	*/
 
-		// rewrites #evalQ HTML element with data from the first (and only) item in the ind'th question in the XML file
-	document.getElementById('evalQ').innerHTML = currQ;
-	document.getElementById('evalR').innerHTML = currR;
+
+/*	Returns a string containing the page name (filename without extension, or Home).
+Inputs: pageURL (string) expects output of window.location.pathname	*/
+function getPage(pageURL) {
+	var pageFile = pageURL.split('/').pop();	// takes the string consisting of everything after the final / character from current URL
+	var selfName = pageFile.split('.').shift();	// takes everything in the page's filename that precedes the extension (typically .shtml)
+	
+	if( selfName === null || (selfName === '' || selfName === 'index') ){	// checks for if the current page is the home page
+		if( ~pageURL.indexOf('pages') )	{ selfName = "sitemap"; }	// site map page and home page are both 'index.shtml' pages, but site map is in pages/ folder
+		else{ selfName = "home"; }
+	}
+	
+	return selfName;
 };
 
-function loadXMLDoc(fileurl) {
+
+/*	Loads an external file for reading in XML formatted data. 	*/
+function loadXMLDoc(fileURL) {
 	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', fileurl, false);
+	xhttp.open('GET', fileURL, false);
 	xhttp.send();
 	
 	var xmltxt = xhttp.responseXML;	
@@ -23,13 +29,8 @@ function loadXMLDoc(fileurl) {
 	return xmltxt;
 };
 
-function toggleAnim(running) {
-	if(running){document.getElementById('emw').src = "images/EM_waves_still.gif"}
-	else{document.getElementById('emw').src = "images/EM_waves.gif"}
-	running = !running;
-	return running;
-};
 
+/*	Generates raised pane full of logos for site map and affiliated organizations.	*/
 function logosPane() {
 	var colorNum = Math.floor(Math.random()*3);	// choose an integer index in the range [0, 3) (3 colors created)
 	
@@ -53,15 +54,86 @@ function logosPane() {
 	}
 };
 
-function selfnav() {
-	var pageurl = window.location.pathname;	// gets current page URL
-	var pagefile = pageurl.split('/').pop();	// takes the string consisting of everything after the final / character from current URL
-	var selfname = pagefile.split('.').shift();	// takes everything in the page's filename that precedes the extension (typically .shtml)
+
+/*	Randomly selects a student evaluation to display and inserts it into an existing table.	*/
+function newEval() {
+	var xmlDoc = loadXMLDoc("supporting/studentEvals.xml");
+	var numEvals = xmlDoc.getElementsByTagName('response').length;	
+	var ind = Math.floor(Math.random()*numEvals);	// ind is random integer in the range [0, numEvals)	
 	
-	if( selfname === null || (selfname === '' || selfname === 'index') ){	// checks for if the current page is the home page
-		selfname = "home";
+	var currQ = xmlDoc.getElementsByTagName('question')[ind].childNodes[0].nodeValue;	// store a single question and response 
+	var currR = xmlDoc.getElementsByTagName('response')[ind].childNodes[0].nodeValue;	
+
+	// rewrites #evalQ HTML element with data from the first (and only) item in the ind'th question in the XML file
+	document.getElementById('evalQ').innerHTML = currQ;
+	document.getElementById('evalR').innerHTML = currR;
+};
+
+
+/*	Rewrites navigation link for the current page to be highlighted, non-clickable text.	*/
+function selfNav() {
+	var pageURL = window.location.pathname;	// gets current page URL
+	var selfName = getPage(pageURL);	// converts URL to filename, sans extension
+	
+	document.getElementById(selfName).className = "navcurrent";	// sets class of current page's link to be highlighted
+	document.getElementById(selfName).innerHTML = selfName;	// removes a tag from current page entry in nav list
+};
+
+
+/*	Adds text relevant to each page to the 'description' meta tag	*/
+function setHeader() {
+	var selfName = getPage(window.location.pathname);	// gets page name
+	var pageInfo = "Personal web page for Marshall Styczinski";	// sets default meta description 
+	var banner = "sunset_croagh_patrick.jpg";	// sets default banner image
+	
+	switch(selfName) {
+		case "home":
+			pageInfo += "";	// sets meta description to have additional info for specific pages 
+			banner = "sunset_croagh_patrick.jpg";	// sets banner to page-specific one
+			break;
+		case "teaching":
+			pageinfo += ": teaching";
+			banner = "across_lough_arrow.jpg";
+			break;
+		case "research":
+			pageinfo += ": research";
+			banner = "tower_at_moher.jpg";
+			break;
+		case "experience":
+			pageinfo += ": experience and qualifications";
+			banner = "over_muckross_head.jpg";			
+			break;
+		case "resources":
+			pageinfo += ": helpful resources";
+			banner = "clew_bay_islands.jpg";
+			break;
+		case "personal":
+			pageinfo += ": personal information";
+			banner = "croagh_patrick_together.jpg";
+			break;
+		case "blog":
+			pageinfo += ": blog";
+			banner = "croagh_patrick_peek.jpg";
+			break;
+		case "sitemap":
+			pageinfo += ": site map";
+			banner = "clew_bay_open.jpg";
+			break;
+		case "construction":
+			pageinfo += ": this page under construction";
+			banner = "kerry.jpg";
 	}
 	
-	document.getElementById(selfname).className = "navcurrent";	// sets class of current page's link to be highlighted
-	document.getElementById(selfname).innerHTML = selfname;	// removes a tag from current page entry in nav list
+	banner = "url('banners/" + banner + "')";	// formats banner string to be of the form 'url(banners/the_image.jpg)'
+	document.getElementById('headbanner').style.backgroundImage = banner;
+	document.getElementsByTagName('meta')[1].content = pageInfo;
+};
+
+
+/*	Toggles animation for screen capture of electromagnetic waves simulation.	*/
+function toggleAnim(running) {
+	if(running){document.getElementById('emw').src = "images/EM_waves_still.gif"}
+	else{document.getElementById('emw').src = "images/EM_waves.gif"}
+	running = !running;
+	return running;
 };
