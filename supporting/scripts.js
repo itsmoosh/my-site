471d71494c7e4627f02e38b200912b22f29b12ec
“Marshall <itsmoosh@gmail.com>
2020-05-26 18:25:41 -0700

    Cleaned up formatting in scripts file following adjustments for audio page in navpane

--- supporting/scripts.js
+++ supporting/scripts.js
@@ -154,10 +154,8 @@ function selfNav() {
 	var pageURL = window.location.pathname;	// gets current page URL
 	var selfName = getPage(pageURL);	// converts URL to filename, sans extension
 	
-	//if( selfName !== "audio" ){
-		document.getElementById(selfName).className = "navcurrent";	// sets class of current page's link to be highlighted
-		document.getElementById(selfName).innerHTML = selfName;	// removes a tag from current page entry in nav list
-	//}
+	document.getElementById(selfName).className = "navcurrent";	// sets class of current page's link to be highlighted
+	document.getElementById(selfName).innerHTML = selfName;	// removes a tag from current page entry in nav list
 };
 
 
